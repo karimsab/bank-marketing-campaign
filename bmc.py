4224a1bd4b60e3de5d05f83b93a69d6d1b9d168f
Saber <62601686+karimsab@users.noreply.github.com>
2022-04-27 19:14:47 +0200

    Update bmc.py

--- bmc.py
+++ bmc.py
@@ -81,7 +81,7 @@ if page == "Etude des variables":
     st.dataframe(df)
     st.markdown('Explication des variables :')
     st.write("1 - age : âge du client")
-             st.write("2 - job : métier du client")'''
+    st.write("2 - job : métier du client")'''
              "3 - marital : statut marital du client \n"
              "4 - education : niveau d'étude du client \n"
              "5 - default : si le client à un crédit impayé \n"
